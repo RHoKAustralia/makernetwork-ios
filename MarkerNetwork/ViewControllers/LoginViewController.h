8cace30e508700dbbe2aef1122151b6edbf74eca
Charles <funzho@gmail.com>
2015-12-05 16:05:22 +1100

    added login page controller

---
+++ MarkerNetwork/ViewControllers/LoginViewController.h
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.h
+//  MarkerNetwork
+//
+//  Created by Fangzhou He on 5/12/2015.
+//  Copyright (c) 2015 MarkerNetwork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+- (IBAction)loginButton:(id)sender;
+@end
