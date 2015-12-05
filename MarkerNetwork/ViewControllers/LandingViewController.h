2d330cd2f73f36bd35402098287d2490ac64aeba
Charles <funzho@gmail.com>
2015-12-05 15:35:44 +1100

    finish landingpage

---
+++ MarkerNetwork/ViewControllers/LandingViewController.h
@@ -0,0 +1,16 @@
+//
+//  LandingViewController.h
+//  MarkerNetwork
+//
+//  Created by Fangzhou He on 5/12/2015.
+//  Copyright (c) 2015 MarkerNetwork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LandingViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIImageView *bgImageView;
+@property (strong, nonatomic) IBOutlet UIButton *signup_button;
+@property (strong, nonatomic) IBOutlet UIButton *login_button;
+
+@end
